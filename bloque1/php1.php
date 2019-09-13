e5f1b97ab5a36203364b707112cbcd30809a3fd1
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-09-13 09:48:12 +0200

    Primer ejercicio php comunidad

--- bloque1/php1.php
+++ bloque1/php1.php
@@ -6,7 +6,16 @@
 <body>
 <?php 
     $pisos = 5;
-    $puertas = 15;
+    $puertas = 4;
+    
+    echo "Aqui se muestran las viviendas de la comunidad";
+    for ($i = 1; $i <= $pisos ; $i++){
+        for ($j = 1; $j <= $puertas; $j++){
+            echo "Piso $i - Puerta $j<br>";
+        }
+    }
+    
+
 
 ?>
 
