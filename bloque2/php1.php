880f1b09372533f2a9932211bfffa7eec90dc543
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-09-30 13:58:40 +0200

    primer ejercicio del bloque 2 sin terminar para hacer en casa

---
+++ bloque2/php1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>2 array</title>
+</head>
+<body>
+
+<?php 
+   $array1 = [0,1,2,3,4,5,6,7,8,9];
+   $array2 = [];
+
+   for ($i = 0; $i<10; $i++){
+       for ($j = 1; $j<=$i; $j++){
+           $j*
+       }
+   }
+
+?>
+
+
+</body>
+</html> 
