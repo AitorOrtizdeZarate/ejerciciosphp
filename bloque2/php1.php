500ead88c0579f50ef43c21e497f613c31936239
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-11-04 19:50:12 +0100

    bloque 2 ejercicio 1 sin terminar

--- bloque2/php1.php
+++ bloque2/php1.php
@@ -5,17 +5,38 @@
 </head>
 <body>
 
-<?php 
-   $array1 = [0,1,2,3,4,5,6,7,8,9];
-   $array2 = [];
-
-   for ($i = 0; $i<10; $i++){
-       for ($j = 1; $j<=$i; $j++){
-           $j*
-       }
-   }
-
-?>
+<?php
+    $array1 = [];
+    $array2 = [];
+    $num;
+    $factorial = 1;
+    
+    for ($i = 0; $i < 10; $i++){
+        $array1[$i] = $i;
+    }
+    foreach ($array1 as $final1){
+        echo "$final1 ";
+        
+    }
+    echo "<br>";
+    for ($i = 0; $i < count($array1); $i++) {
+        $array2[$i] = factorial($array1[$i]);
+    }
+    function factorial($num) {
+        $num2 = 1;
+        for($i = 1; $i <= $num; $i++){
+            $num2 = $num2 * $i;
+        }
+        return $num2;
+    }
+    
+    
+    
+    foreach ($array2 as $final2){
+        echo "$final2 ";
+        
+    }
+	?>
 
 
 </body>
