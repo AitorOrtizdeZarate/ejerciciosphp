4fc1c99bbe51f04c527117963274da896e9ab0ec
AitorOrtizdeZarate <aortizdezaratega18dw@ikzubirimanteo.com>
2019-11-13 20:28:43 +0100

    ejercicio 9 bloque 2

---
+++ bloque2/php9.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bloque 2 ejercicio 8</title>
+</head>
+<body>
+	<?php
+		function calcular($lado){
+			if($lado < 0){
+				throw new Exception ($lado."Es negativo");
+			}
+			else{
+				return pow($lado, 2);
+			}
+		}
+
+		$randNum = [];
+
+		for($i=0; $i<4; $i++){
+			array_push($randNum, rand(0,50));
+		} 
+
+		array_push($randNum, rand(-50,-1));
+
+		for($i = 0; $i < sizeof($randNum); $i++){
+			echo "El area es: ".calcular($randNum[$i])."<br>";
+		}
+	?>
+</body>
+</html>
